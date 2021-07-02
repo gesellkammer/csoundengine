e0221404423ad0e247a7373fc3d1cf0ed9483672
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2021-07-02 13:29:39 +0200

    some test files

---
+++ test/simple.py
@@ -0,0 +1,15 @@
+import logging
+logging.basicConfig(level="DEBUG")
+import csoundengine
+e = csoundengine.Engine()
+s = e.session()
+s.defInstr("foo", r"""
+imidi = p5
+ifreq = mtof:i(imidi)
+print imidi, ifreq
+a0 oscili 0.1, mtof:i(imidi)
+outch 1, a0
+""")
+import time
+s.sched("foo", 0, 4, pargs=[60])
+time.sleep(4)
